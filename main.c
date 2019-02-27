e6386f6bcb7507a27d1354bec34a80fb6ac52914
kbolek <kbolekagh@gmail.com>
2019-02-27 14:00:18 +0100

    1.1 Tworzenie projektu

---
+++ main.c
@@ -0,0 +1,13 @@
+/*1. Obsluga srodowiska Keil - 1.1 tworzenie projektu */
+int iPlusCounter;
+int iMinusCounter;
+int iLoopCtr;
+
+int main(){
+	iPlusCounter=10;
+	iMinusCounter=10;
+	for (iLoopCtr=0;iLoopCtr<5;iLoopCtr++){
+		iPlusCounter=iPlusCounter+3;
+		iMinusCounter=iMinusCounter-3;
+	}
+}

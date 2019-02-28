d12de6c8c66342eaf81b84172ceef39dc9a23b07
kbolek <kbolekagh@gmail.com>
2019-02-28 22:31:09 +0100

    4.2 cwiczenie podpunkt 6

--- delay.c
+++ delay.c
@@ -1,11 +1,12 @@
-/*4.2 cwiczenia podpunktn 4,5 */
+/*4.2 cwiczenia podpunkt 6 */
 #include <LPC21xx.H>
+#define MILI 4615
 
-void Delay(){
+void Delay(int ile){
 	unsigned long int i;
-	//dla mojego kompilatora 1s to 6mln iteracji
+	//dla mojego kompilatora 1 ms to 4615 iteracji
 	char a;
-	for(i=0; i<6000000;i++){
+	for(i=0; i<(ile*MILI);i++){
 		a++;
 	}
 }
@@ -15,7 +16,7 @@ int main(){
 	IO1DIR = 0x10000;
 	//ustawienie pinu 16 portu P1 na 1
 	IO1SET = 0x10000;
-	Delay();
+	Delay(1001);
 	//ustawienie pinu 16 portu P1 na 0
 	IO1CLR = 0x10000;
 

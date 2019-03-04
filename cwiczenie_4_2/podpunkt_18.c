fb3683f951e42d7853190f86cbe7364bd6dd4c29
kbolek <48052345+kbolek@users.noreply.github.com>
2019-03-04 22:25:11 +0100

    Poprawa konwencji zapisu.

--- cwiczenie_4_2/podpunkt_18.c
+++ cwiczenie_4_2/podpunkt_18.c
@@ -4,22 +4,23 @@
 #define S1 0x20
 #define S2 0x40
 #define S3 0x80
-#define LED0_bm  0xA0//16 bit ma wartosc 1
-#define LED1_bm  0x20000 //17 bit ma wartosc 1
-#define LED2_bm  0x40000 // 18 bit ma wartosc 1
-#define LED3_bm 0x80000 //19 bit ma wartosc 1
+#define LED0_bm  0xA0
+#define LED1_bm  0x20000 
+#define LED2_bm  0x40000 
+#define LED3_bm 0x80000 
 
-enum ButtonState {RELEASED,PRESSED}; //RELEASED => 0, PRESSED => 1
+
 /*Funkcja zwracajaca stan przycisku S1 */
-int ReadButton1(){
-	enum ButtonState ReturnNumber = RELEASED;
-	ReturnNumber = ((IO0PIN&S1)== S1) ? PRESSED: RELEASED;
-	return ReturnNumber;
+int iReadButton1(){
+	enum ButtonState {RELEASED,PRESSED}; 
+	enum ButtonState iReturnNumber = RELEASED;
+	iReturnNumber = ((IO0PIN&S1)== S1) ? PRESSED: RELEASED;
+	return iReturnNumber;
 }
 
 /*Funkcja ustawiajaca wszystkie piny przyciskow S1-S4 na wejscie */
 void KeyboardInit(){
-	IO0DIR = IO0DIR|S0|S1|S2|S3; //bez modyfikacji innych bitow rejestru
+	IO0DIR = IO0DIR|S0|S1|S2|S3; 
 }
 
 /*Funkcja zapalajaca diode LED o podanym argumencie */
@@ -36,10 +37,10 @@ void LedOn(int Number){
 	
 }
 int main(){
-	int GiveANumber;
+	int iGiveANumber;
 	IO1DIR = IO1DIR|LED0_bm|LED1_bm;
 	KeyboardInit();
 	IO0SET = IO0SET|S1;
-	GiveANumber = ReadButton1();
-	LedOn(GiveANumber);	
+	iGiveANumber = iReadButton1();
+	LedOn(iGiveANumber);	
 }

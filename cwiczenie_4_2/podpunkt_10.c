58025ef07d914a4cee0b5dfa162f6565b32414cd
kbolek <48052345+kbolek@users.noreply.github.com>
2019-03-04 22:14:59 +0100

    Update podpunkt_10.c

--- cwiczenie_4_2/podpunkt_10.c
+++ cwiczenie_4_2/podpunkt_10.c
@@ -1,16 +1,12 @@
 /*4.2 cwiczenia podpunktu 11 */
 #include <LPC21xx.H>
 #define MILI 4615
-#define LED0_bm  0x10000 
-#define LED1_bm  0x60000 
-#define LED2_bm  0x40000 
 #define LED3_bm 0x80000 
 
-/*Nastepuje zapalenie 4 diod */
+/*Miganie diody LED3 */
 int main(){
-	IO1DIR = IO1DIR|LED0_bm|LED1_bm|LED2_bm|LED3_bm;
-	IO1SET = IO1SET|LED0_bm;
-	IO1SET = IO1SET|LED1_bm;
-	IO1SET = IO1SET|LED2_bm;
-	IO1SET = IO1SET|LED3_bm;
+	while(1){
+		IO1SET = IO1SET|LED3_bm;
+		IO1CLR = IO1SET|LED3_bm;	
+	}
 }

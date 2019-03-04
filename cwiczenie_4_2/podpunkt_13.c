ae75e86675959ef379df4c106362684f48e7ae08
kbolek <48052345+kbolek@users.noreply.github.com>
2019-03-04 22:17:04 +0100

    Poprawa konwencji zapisu.

--- cwiczenie_4_2/podpunkt_13.c
+++ cwiczenie_4_2/podpunkt_13.c
@@ -7,26 +7,26 @@
 #define LED2_bm  0x40000 // 18 bit ma wartosc 1
 #define LED3_bm 0x80000 //19 bit ma wartosc 1
 
-void Delay(int ile){
-	unsigned long int i;
-	//dla mojego kompilatora 1 ms to 4615 iteracji
-	char a;
-	for(i=0; i<(ile*MILI);i++){
-		a++;
+/*Funkcja odpowiedzialna za opoznienie
+dla mojego kompilatora 1 ms to 4615 iteracji*/
+void Delay(int iHowLong){
+	unsigned long int uiIntCounter;
+	char cCharIncrementation;
+	for(uiIntCounter=0; uiIntCounter<(iHowLong*MILI);uiIntCounter++){
+		cCharIncrementation++;
 	}
 }
 
-/*Funkcja która ustawia piny podpiete do LED0-LED3 na wyjsciowe
+/*Funkcja ktÃ³ra ustawia piny podpiete do LED0-LED3 na wyjsciowe
 i zapala LED0*/
 void LedInit(){
 		IO1DIR = IO1DIR|LED0_bm|LED1_bm|LED2_bm|LED3_bm;
-		IO1SET = IO1SET|LED0_bm;
-	
+		IO1SET = IO1SET|LED0_bm;	
 }
 
+/* Funkcja przesuwajaca punkty swietlne led 0-3 0-3 itd. */
 int main(){
 	while(1){
-		//przesuwanie punktu swietlnego led 0-3 0-3 itd.
 		LedInit();
 		Delay(250);
 		IO1CLR = IO1CLR|LED0_bm;

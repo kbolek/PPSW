dd956d3cdc161efb7bd32479e83947865445698a
kbolek <48052345+kbolek@users.noreply.github.com>
2019-03-04 22:06:31 +0100

    Poprawa konwencji zapisu.

--- cwiczenie_4_2/podpunkt_4_5.c
+++ cwiczenie_4_2/podpunkt_4_5.c
@@ -1,22 +1,20 @@
 /*4.2 cwiczenia podpunkt 4,5 */
 #include <LPC21xx.H>
 
-void Delay(){
-	unsigned long int i;
-	//dla mojego kompilatora 1s to 6mln iteracji
-	char a;
-	for(i=0; i<6000000;i++){
-		a++;
+/*Funkcja odpowiedzialna za opoznienie
+dla mojego kompilatora 1 ms to 4615 iteracji*/
+void Delay(int iHowLong){
+	unsigned long int uiIntCounter;
+	char cCharIncrementation;
+	for(uiIntCounter=0; uiIntCounter<(iHowLong*MILI);uiIntCounter++){
+		cCharIncrementation++;
 	}
 }
 
 int main(){
-	//ustawienie pinu 16 portu P1 jako wyjsciowy
 	IO1DIR = 0x10000;
-	//ustawienie pinu 16 portu P1 na 1
 	IO1SET = 0x10000;
 	Delay();
-	//ustawienie pinu 16 portu P1 na 0
 	IO1CLR = 0x10000;
 
 }
